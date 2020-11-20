e5225b9076493474be419f413393d69c7a289e67
Jüri Kajava <73351309+kajava-juri@users.noreply.github.com>
2020-11-20 11:12:10 +0200

    finished

--- Tic tac toe.py
+++ Tic tac toe.py
@@ -36,8 +36,12 @@ def input_marker(board, marker, position):
 def space_check(board, position):
     return board[position] == "#"
 
-def space_input(board):
-    choice = int(input("chose place from 1-9: "))
+def space_input(board, i, players):
+    if i % 2 == 0:
+        player = players[1]
+    if i % 2 == 1:
+        player = players[0]
+    choice = int(input(f"{player}'s turn\nchose place from 1-9: "))
     while not space_check(board, int(choice)):
         choice = int(input("This space isn't free. Choose another one from 1-9: "))
     return choice
@@ -48,13 +52,39 @@ def full_board_check(board):
         if x == '#':
             filteredBoard.append(x)
     return (len(filteredBoard) == 1)
+def win_check(board, marker):
+    if board[1] == board[2] == board[3] == marker:
+        return True
+    if board[4] == board[5] == board[6] == marker:
+        return True
+    if board[7] == board[8] == board[9] == marker:
+        return True
+    if board[1] == board[4] == board[7] == marker:
+        return True
+    if board[2] == board[5] == board[8] == marker:
+        return True
+    if board[3] == board[6] == board[9] == marker:
+        return True
+    if board[1] == board[5] == board[9] == marker:
+        return True
+    if board[3] == board[5] == board[7] == marker:
+        return True
+    return False
+
+def replay():
+    question = input("Do you want to play again 'y' or 'n': ")
+    if question == "y":
+        return True
+    if question == "n":
+        return False
+    
 
 while True:
     i = 1
     players = player_side()
     game_on = full_board_check(board)
     while not game_on:
-        position = space_input(board)
+        position = space_input(board, i, players)
         if i % 2 == 0:
             marker = players[1]
         else:
@@ -63,8 +93,14 @@ while True:
         DisplayBoard(board)
         i += 1
         game_on = full_board_check(board)
-    #replay
-    #
-    #
-    #.......
+        if win_check(board, marker):
+            print(f"{marker} won!")
+            break
+    if game_on:
+        print("It's a draw! Board id full")
+    if not replay():
+        break
+    else:
+        i = 1
+        board = ['#'] * 10
 
